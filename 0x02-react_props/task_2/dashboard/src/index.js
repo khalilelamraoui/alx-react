92e0473cd147c6dcb14a6b7a0ff1189db3b765a6
khalilelamraoui <khalilelam16@gmail.com>
2024-07-26 20:54:50 +0100

    fixed index.js

--- 0x02-react_props/task_2/dashboard/src/index.js
+++ 0x02-react_props/task_2/dashboard/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from './Notifications/Notifications';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<div className='root-notifications'>
-			<Notifications />
-		</div>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
